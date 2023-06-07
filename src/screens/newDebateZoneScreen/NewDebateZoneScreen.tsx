905dfc4400400ec417c388c383981fe80c27fcad
Ion Ermuratii <ermuratiitep@gmail.com>
2023-06-07 02:35:18 +0000

    new-debate-zone

--- src/screens/newDebateZoneScreen/NewDebateZoneScreen.tsx
+++ src/screens/newDebateZoneScreen/NewDebateZoneScreen.tsx
@@ -1,9 +1,25 @@
-import { Text } from 'react-native';
+import { request } from '../../apiClient/apiClient';
+import { CreatedDebateZone, NewDebateZone } from '../../types/debateZone';
+import Toast from 'react-native-root-toast';
+import { NewDebateZoneComponent } from '../../components/debateZone/newDetateZone/NewDebateZoneComponent';
 
 export const NewDebateZoneScreen = () => {
+    const onSubmit = async (data: NewDebateZone) => {
+        const createdDebateZone: CreatedDebateZone =
+            await request<CreatedDebateZone>(
+                'POST',
+                '/debate-zone/create',
+                data,
+            );
+
+        Toast.show(`Debate zone "${createdDebateZone.title}" created`, {
+            duration: Toast.durations.LONG,
+        });
+    };
+
     return (
         <>
-            <Text>NewDebateZoneScreen component</Text>
+            <NewDebateZoneComponent onSubmit={onSubmit} />
         </>
     );
 };
